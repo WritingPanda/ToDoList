14bfaf23bac78569aba7970b3bb60846891f9366
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-10-08 13:33:36 -0500

    Messing around with XAML and C#. Moving towards a todolist app in said languages.

--- ToDoList/MainWindow.xaml.cs
+++ ToDoList/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +26,74 @@ namespace ToDoList
         {
             InitializeComponent();
         }
+
+        void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.typeName.Text == "")
+            {
+                this.result.Text = "Nothing was added. Please enter your name above.";
+            }
+            else if (this.typeName.Text == "Thomas")
+            {
+                this.result.Text = "Enjoy Austin, " + this.typeName.Text + "... Whatever.";
+            }
+            else
+            {
+                this.result.Text = "Hello, " + this.typeName.Text + "! Hope you are having a wonderful day!";
+            }
+
+            try
+            {
+                File.OpenRead("NonExistentFile");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Finally has been reached.");
+            }
+        }
+
+        void OpenFilesMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void OpenDirMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void ClearInputMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.typeName.Text = "";
+        }
+
+        void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        void CopyMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void PasteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.typeName.Text = Clipboard.GetText();
+        }
+
+        private static readonly Random rnd = new Random();
+
+        void SurpriseClick(object sender, RoutedEventArgs e)
+        {
+            string[] names = new string[7] { "Paul", "Omar", "David", "Casey", "Kevin", "Thomas", "Omar rocks!" };
+            int arrayNum = rnd.Next(names.Length);
+            this.typeName.Text = names[arrayNum];
+        }
+
     }
 }
